8bdbeaffd246a807a5f156a274e1b0af87130f13
Paul Hammant <paul@hammant.org>
2020-02-01 08:24:18 +0000

    add in remainder of junit tests

--- src/test/kotlin/servirtium/http4k/java/ClimateApiTests.java
+++ src/test/kotlin/servirtium/http4k/java/ClimateApiTests.java
@@ -12,6 +12,43 @@ public interface ClimateApiTests {
 
     @Test
     default void averageRainfallForGreatBritainFrom1980To1999Exists() {
-        assertEquals(new ClimateApi(uri()).getAveAnnualRainfall(1980, 1999, "gbr"), 988.8454972331015);
+        assertEquals(988.8454972331015, new ClimateApi(uri()).getAveAnnualRainfall(1980, 1999, "gbr"));
     }
+
+    @Test
+    default void averageRainfallForFranceFrom1980To1999Exists() {
+        assertEquals(913.7986955122727, new ClimateApi(uri()).getAveAnnualRainfall(1980, 1999, "fra"));
+    }
+
+    @Test
+    default void averageRainfallForEgyptFrom1980To1999Exists() {
+        assertEquals(54.58587712129825, ClimateApi(uri).getAveAnnualRainfall(1980, 1999, "egy"));
+    }
+
+    @Test
+    default void averageRainfallForGreatBritainFrom1985To1995DoesNotExist() {
+        // wrong date ranges just return an empty list of data -
+        try {
+            new ClimateApi(uri()).getAveAnnualRainfall(1985, 1995, "gbr");
+        } catch (ClimateApi.BadDateRange e) {
+            assertEquals("date range 1985-1995 not supported", e.getLocalizedMessage());
+        }
+
+    }
+
+    @Test
+    default void averageRainfallForMiddleEarthFrom1985To1995DoesNotExist() {
+        // wrong date ranges just return an empty list of data -
+        try {
+            new ClimateApi(uri()).getAveAnnualRainfall(1980, 1999, "mde");
+        } catch (ClimateApi.BadDateRange e) {
+                    assertEquals("mde not recognized by climateweb", e.getLocalizedMessage());
+        }
+    }
+
+    @Test
+    default void averageRainfallForGreatBritainAndFranceFrom1980To1999CanBeCalculatedFromTwoRequests() {
+        assertEquals(951.3220963726872, new ClimateApi(uri()).getAveAnnualRainfall(1980, 1999, "gbr", "fra"))
+    }
+
 }
