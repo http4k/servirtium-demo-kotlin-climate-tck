936d776d47ead3c54a019ed79b4de12f888bbb8c
David Denton <denton.david@gmail.com>
2020-02-02 09:17:22 +0000

    moved java version of tests to java source package

--- src/test/java/servirtium/http4k/java/ClimateInteractionOptions.java
+++ src/test/java/servirtium/http4k/java/ClimateInteractionOptions.java
@@ -7,6 +7,8 @@ import org.http4k.servirtium.InteractionOptions;
 import org.jetbrains.annotations.NotNull;
 
 public class ClimateInteractionOptions implements InteractionOptions {
+
+    @NotNull
     @Override
     public Request modify(@NotNull Request request) {
         return request
@@ -15,6 +17,7 @@ public class ClimateInteractionOptions implements InteractionOptions {
                 .replaceHeader("Date", "Tue, 28 Jan 2020 14:15:55 GMT");
     }
 
+    @NotNull
     @Override
     public Response modify(@NotNull Response response) {
         return response
